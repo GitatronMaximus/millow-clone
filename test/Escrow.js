fef4866f54d5ee78b391a4a4a7806ed490434e83
Gitatron Maximus <blockhead.onchain@gmail.com>
2024-02-04 18:12:01 -0500

    Added incorrect escrow return fail case and success blocks

--- test/Escrow.js
+++ test/Escrow.js
@@ -39,87 +39,94 @@ describe('Escrow', () => {
         await transaction.wait()
 
         const currentOwner = await realEstate.ownerOf(1)
-        console.log("Current owner: ", currentOwner)  
     })
 
     describe('Deployment', () => {
-        it('Returns NFT address', async () => {
-            const result = await escrow.nftAddress()
-            expect(result).to.be.equal(realEstate.address)
+        describe('Success', () => {
+
+            it('Returns NFT address', async () => {
+                const result = await escrow.nftAddress()
+                expect(result).to.be.equal(realEstate.address)
+            })
+
+            it('Returns seller', async () => {
+                const result = await escrow.seller()
+                expect(result).to.be.equal(seller.address)
+            })
+
+            it('Returns inspector', async () => {
+                const result = await escrow.inspector()
+                expect(result).to.be.equal(inspector.address)
+            })
+
+            it('Returns lender', async () => {
+                const result = await escrow.lender()
+                expect(result).to.be.equal(lender.address)
+
+                const currentOwner = await realEstate.ownerOf(1)
+            })  
         })
-
-        it('Returns seller', async () => {
-            const result = await escrow.seller()
-            expect(result).to.be.equal(seller.address)
-        })
-
-        it('Returns inspector', async () => {
-            const result = await escrow.inspector()
-            expect(result).to.be.equal(inspector.address)
-        })
-
-        it('Returns lender', async () => {
-            const result = await escrow.lender()
-            expect(result).to.be.equal(lender.address)
-
-            const currentOwner = await realEstate.ownerOf(1)
-            console.log("Current owner: ", currentOwner)
-        })  
-
         describe('Failure', async () => {
             //fail code
         })
     })
 
     describe('Listing', () => {
-        it('Updates as listed', async () => {
-            const result = await escrow.isListed(1)
-            expect(result).to.be.equal(true)
+        describe('Success', () => {
+
+            it('Updates as listed', async () => {
+                const result = await escrow.isListed(1)
+                expect(result).to.be.equal(true)
+            })
+
+            it('Returns buyer', async () => {
+                const result = await escrow.buyer(1)
+                expect(result).to.be.equal(buyer.address)
+            })
+
+            it('Returns purchase price', async () => {
+                const result = await escrow.purchasePrice(1)
+                expect(result).to.be.equal(tokens(10))
+            })
+
+            it('Returns escrow amount', async () => {
+                const result = await escrow.escrowAmount(1)
+                expect(result).to.be.equal(tokens(5))
+            })
+
+            it('Updates ownership', async () => {
+                expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address)
+                const currentOwner = await realEstate.ownerOf(1)
+            })
         })
-
-        it('Returns buyer', async () => {
-            const result = await escrow.buyer(1)
-            expect(result).to.be.equal(buyer.address)
-        })
-
-        it('Returns purchase price', async () => {
-            const result = await escrow.purchasePrice(1)
-            expect(result).to.be.equal(tokens(10))
-        })
-
-        it('Returns escrow amount', async () => {
-            const result = await escrow.escrowAmount(1)
-            expect(result).to.be.equal(tokens(5))
-        })
-
-        it('Updates ownership', async () => {
-            expect(await realEstate.ownerOf(1)).to.be.equal(escrow.address)
-
-            const currentOwner = await realEstate.ownerOf(1)
-        console.log("Current owner: ", currentOwner)
-        })
-
           
-
         describe('Failure', async () => {
-            //fail code
+            let incorrectEscrow
+
+            it('Fails to return correct escrow amount', async () => {
+            const result = await escrow.escrowAmount(1)
+            const incorrectEscrow = tokens(3)
+
+            expect(result).to.not.equal(incorrectEscrow)       
+            })
         })
     })
        
     describe('Deposits', () => {
-        beforeEach(async () => {
-            const transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) })
-            await transaction.wait()
+        describe('Success', () => {
+
+            beforeEach(async () => {
+                const transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) })
+                await transaction.wait()
+            })
+
+            it('Updates contract balance', async () => {
+                const result = await escrow.getBalance()
+                expect(result).to.be.equal(tokens(5))
+
+                const currentOwner = await realEstate.ownerOf(1)
+            })
         })
-
-        it('Updates contract balance', async () => {
-            const result = await escrow.getBalance()
-            expect(result).to.be.equal(tokens(5))
-
-            const currentOwner = await realEstate.ownerOf(1)
-         console.log("Current owner: ", currentOwner) 
-        })
-
          
     
         describe('Failure', async () => {
@@ -133,19 +140,18 @@ describe('Escrow', () => {
         })
     })
     describe('Inspection', () => {
-        beforeEach(async () => {
-            const transaction = await escrow.connect(inspector).updateInspectionStatus(1, true)
-            await transaction.wait()
+        describe('Success', () => {
+
+            beforeEach(async () => {
+                const transaction = await escrow.connect(inspector).updateInspectionStatus(1, true)
+                await transaction.wait()
+            })
+
+            it('Updates inspection status', async () => {
+                const result = await escrow.inspectionPassed(1)
+                expect(result).to.be.equal(true)
+            })
         })
-
-        it('Updates inspection status', async () => {
-            const result = await escrow.inspectionPassed(1)
-            expect(result).to.be.equal(true)
-
-            const currentOwner = await realEstate.ownerOf(1)
-        console.log("Current owner: ", currentOwner) 
-        })
-
          
 
         describe('Failure', async () => {
@@ -154,24 +160,26 @@ describe('Escrow', () => {
     })
 
     describe('Approval', () => {
-        beforeEach(async () => {
-            let transaction = await escrow.connect(buyer).approveSale(1)
-            await transaction.wait()
+        describe('Success', () => {
 
-            transaction = await escrow.connect(seller).approveSale(1)
-            await transaction.wait()
+            beforeEach(async () => {
+                let transaction = await escrow.connect(buyer).approveSale(1)
+                await transaction.wait()
 
-            transaction = await escrow.connect(lender).approveSale(1)
-            await transaction.wait()
-        })
+                transaction = await escrow.connect(seller).approveSale(1)
+                await transaction.wait()
 
-        it('Updates approval status', async () => {
-            expect(await escrow.approval(1, buyer.address)).to.be.equal(true)
-            expect(await escrow.approval(1, seller.address)).to.be.equal(true)
-            expect(await escrow.approval(1, lender.address)).to.be.equal(true)
+                transaction = await escrow.connect(lender).approveSale(1)
+                await transaction.wait()
+            })
 
-            const currentOwner = await realEstate.ownerOf(1)
-            console.log("Current owner: ", currentOwner)            
+            it('Updates approval status', async () => {
+                expect(await escrow.approval(1, buyer.address)).to.be.equal(true)
+                expect(await escrow.approval(1, seller.address)).to.be.equal(true)
+                expect(await escrow.approval(1, lender.address)).to.be.equal(true)
+
+                const currentOwner = await realEstate.ownerOf(1)
+            })
         })
 
         describe('Failure', async () => {
@@ -180,34 +188,37 @@ describe('Escrow', () => {
     })
 
     describe('Sale', () => {
-        beforeEach(async () => {
-            let transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) })
-            await transaction.wait()
+        describe('Success', () => {
 
-            transaction = await escrow.connect(inspector).updateInspectionStatus(1, true)
-            await transaction.wait()
+            beforeEach(async () => {
+                let transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) })
+                await transaction.wait()
 
-            transaction = await escrow.connect(buyer).approveSale(1)
-            await transaction.wait()
+                transaction = await escrow.connect(inspector).updateInspectionStatus(1, true)
+                await transaction.wait()
 
-            transaction = await escrow.connect(seller).approveSale(1)
-            await transaction.wait()
+                transaction = await escrow.connect(buyer).approveSale(1)
+                await transaction.wait()
 
-            transaction = await escrow.connect(lender).approveSale(1)
-            await transaction.wait()
+                transaction = await escrow.connect(seller).approveSale(1)
+                await transaction.wait()
 
-            await lender.sendTransaction({ to: escrow.address, value: tokens(5) })
+                transaction = await escrow.connect(lender).approveSale(1)
+                await transaction.wait()
 
-            transaction = await escrow.connect(seller).finalizeSale(1)
-            await transaction.wait()
-        })
+                await lender.sendTransaction({ to: escrow.address, value: tokens(5) })
 
-        it('Updates ownership', async () => {
-            expect(await realEstate.ownerOf(1)).to.be.equal(buyer.address)
-        })
+                transaction = await escrow.connect(seller).finalizeSale(1)
+                await transaction.wait()
+            })
 
-        it('Updates balance', async () => {
-            expect(await escrow.getBalance()).to.be.equal(0)
+            it('Updates ownership', async () => {
+                expect(await realEstate.ownerOf(1)).to.be.equal(buyer.address)
+            })
+
+            it('Updates balance', async () => {
+                expect(await escrow.getBalance()).to.be.equal(0)
+            })
         })
 
         describe('Failure', async () => {
@@ -215,7 +226,6 @@ describe('Escrow', () => {
 
             let transaction = await escrow.connect(buyer).depositEarnest(1, { value: tokens(5) })
             await transaction.wait()
-            console.log("Buyer address: ", buyer.address)
 
             transaction = await escrow.connect(inspector).updateInspectionStatus(1, false)
             await transaction.wait()           
@@ -225,10 +235,8 @@ describe('Escrow', () => {
             it('Fails to update ownership due to unmet condition', async () => {
 
             await expect(escrow.connect(seller).finalizeSale(1)).to.be.reverted
-            console.log("Seller address: ", seller.address)
 
             const currentOwner = await realEstate.ownerOf(1)
-            console.log("Current owner: ", currentOwner)
 
             expect(await realEstate.ownerOf(1)).to.not.equal(buyer.address)
             })
